391dc4d17e9115941e43c4b8d70f7f1dea7adfc7
Fer Ques <402066@sistemas.frc.utn.edu.ar>
2025-06-27 18:02:57 -0300

    Segundo cambio usuario

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace RedSismicaWinForms
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Cambiá aquí para crear un usuario y pasar el gestor:
-            Usuario analistaEnSismos = new Usuario("Analista Principal");
+            Usuario analistaEnSismos = new Usuario("Analista Principal en sismos");
             GestorRegistrarResultado gestor = new GestorRegistrarResultado(analista);
 
             Application.Run(new PantRegistrarResultado(gestor));
